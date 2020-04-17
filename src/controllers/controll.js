fccd2df79bd1f3835073a4d60cba091cb88882ce
koushith <koushith97@gmail.com>
2020-04-18 01:39:48 +0530

    axios

--- src/controllers/controll.js
+++ src/controllers/controll.js
@@ -1,3 +1,13 @@
+// import axios to makr req
+
+const axios = require("axios");
+
+// API
+
+const API_KEYS = "3f4c8efde8dfa7274ccb444191aa597f";
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEYS}`;
+
 //  we are ready to export. controllers main job is to controll the flow
 
 exports.renderHomePage = (req, res) => {
@@ -9,6 +19,13 @@ exports.renderAboutPage = (req, res) => {
 };
 
 exports.rendergetWeather = (req, res) => {
-  console.log(req);
-  res.send(`you types ${req.body.city}`);
+  const city = req.body.city; //city is coming from body of req. (replacement for value.)
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
